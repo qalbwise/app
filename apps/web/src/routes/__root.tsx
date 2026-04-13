e7ba17e3a4490bcf3dfde6a2745ffa32b7430b33
up2dul <up2dul@gmail.com>
2026-04-13 15:24:39 +0700

    feat(web/routes): tanstack query provider

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,8 +1,20 @@
 import { TanStackDevtools } from "@tanstack/react-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
 import "../styles.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      retry: false,
+    },
+  },
+});
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -11,7 +23,9 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
       <TanStackDevtools
         config={{
           position: "bottom-right",
