84e260d1420f13c3efd2ba9e8b00d4b8f4eb45f8
up2dul <up2dul@gmail.com>
2026-05-12 21:57:03 +0700

    feat(web/fonts): replace import url fonts with fontsource

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,7 +3,14 @@ import ReactDOM from "react-dom/client";
 
 import { routeTree } from "./routeTree.gen";
 import "@fontsource-variable/inter/wght.css";
+import "@fontsource-variable/noto-naskh-arabic/wght.css";
 import "@fontsource-variable/plus-jakarta-sans/wght.css";
+import "@fontsource/amiri/400.css";
+import "@fontsource/amiri/700.css";
+import "@fontsource/amiri/400-italic.css";
+import "@fontsource/amiri/700-italic.css";
+import "@fontsource/scheherazade-new/400.css";
+import "@fontsource/scheherazade-new/700.css";
 
 const router = createRouter({
   routeTree,
