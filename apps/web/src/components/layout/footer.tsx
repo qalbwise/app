5a7e369da802d0d5d0c715e5fa71453892ccbd51
up2dul <up2dul@gmail.com>
2026-05-14 13:52:52 +0700

    feat(web/layout): re-layouting the footer contents

--- apps/web/src/components/layout/footer.tsx
+++ apps/web/src/components/layout/footer.tsx
@@ -2,21 +2,10 @@ const year = new Date().getFullYear();
 
 export const Footer = () => {
   return (
-    <footer className="main-wrap flex flex-col gap-4 border-black/5 border-t bg-white/95 py-5 text-muted-foreground text-sm backdrop-blur-sm md:gap-1 lg:gap-0">
-      <div className="flex flex-col justify-between gap-2 md:flex-row">
-        <p>&copy; {year} Qalbwise</p>
+    <footer className="main-wrap flex flex-wrap justify-between gap-x-6 gap-y-4 border-black/5 border-t bg-white/95 py-5 text-muted-foreground text-sm backdrop-blur-sm">
+      <p className="order-1 md:order-0">&copy; {year} Qalbwise</p>
 
-        <a
-          href="https://github.com/qalbwise/app"
-          target="_blank"
-          rel="noreferrer"
-          className="transition-all hover:text-foreground hover:underline"
-        >
-          GitHub
-        </a>
-      </div>
-
-      <p className="md:text-center">
+      <p className="order-2 md:order-1">
         Built for the{" "}
         <a
           href="https://launch.provisioncapital.com/quran-hackathon"
@@ -28,6 +17,15 @@ export const Footer = () => {
         </a>{" "}
         Hackathon
       </p>
+
+      <a
+        href="https://github.com/qalbwise/app"
+        target="_blank"
+        rel="noreferrer"
+        className="order-0 transition-all hover:text-foreground hover:underline md:order-2"
+      >
+        GitHub
+      </a>
     </footer>
   );
 };
