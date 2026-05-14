994342fbe1c309ef0b0a618701fe06e2f33e28ba
up2dul <up2dul@gmail.com>
2026-05-14 16:37:53 +0700

    feat(web/comps): shadcn button and dialog bit adjustments

--- apps/web/src/components/ui/button.tsx
+++ apps/web/src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground enabled:hover:bg-secondary-foreground",
+          "bg-primary text-primary-foreground enabled:hover:bg-secondary-foreground dark:enabled:hover:bg-nugget-700",
         outline:
           "border-border bg-background hover:text-foreground enabled:hover:bg-secondary aria-expanded:bg-muted aria-expanded:text-foreground dark:border-input dark:bg-input/30 dark:hover:bg-input/50",
         secondary:
